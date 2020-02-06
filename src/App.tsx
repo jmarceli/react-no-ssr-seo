c68e77a70ba2245b0742480a8ed39a99eee8374e
Jan Grzegorowski <grzegorowski@gmail.com>
2020-02-06 23:41:56 +0100

    Create simple app with @reach/router and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, RouteComponentProps, Link } from '@reach/router';
 import './App.css';
+
+const Page: React.FC<RouteComponentProps> = () => (
+  <div>
+    <p>
+      This is an example page which is not a root page so a crawler should be
+      clever
+      <br />
+      enough to follow internal links and index this content
+    </p>
+  </div>
+);
+
+const Home: React.FC<RouteComponentProps> = () => {
+  return (
+    <div>
+      <p>
+        Simple routing using @reach/router is added to see how other routes will
+        be indexed.
+        <br />
+      </p>
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This is a test project which will show how React applications are
+          indexed by the Google engine.
+          <br />
+          This project has no SSR (Server Side Rendering).
+          <br />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <nav>
+        <div>
+          <Link to="/">Homepage</Link>
+        </div>
+        <div>
+          <Link to="/page">Subpage</Link>
+        </div>
+      </nav>
+      <main>
+        <Router>
+          <Home path="/" />
+          <Page path="/page" />
+        </Router>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
